54d8dc5e672e2a83a406c1a33f3c0273aae4489f
unknown <pavithralakshmi84@gmail.com>
2025-11-28 10:09:33 +0530

    updated

--- React3/src/Components/use_effect.jsx
+++ React3/src/Components/use_effect.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState } from "react";
-import Tickets from "../assets/quote.jpg";
+import { useEffect, useState } from "react";
+import img from "../assets/quote.jpg";
+
 
 const Quote = () => {
-  const [newdatas, setNewDatas] = useState([]);
 
-  const fetchDatas = async () => {
-    try {
-      const datastake = await fetch("https://dummyjson.com/quotes");
-      const res = await datastake.json();
-      const datas = res.quotes; // correct key from API
-      setNewDatas(datas);
-    } catch (err) {
-      console.error("Error fetching quotes:", err);
-    }
-  };
+    const [info,setInfo] = useState([
+      { quote: "...", author: "..." },
+    
+       
+    ])
 
-  useEffect(() => {
-    fetchDatas();
-  }, []);
-
-  return (
-    <>
-    <img src={Tickets} alt="Tickets" style={{ width: 300, marginTop: 20 }} />
-      {newdatas.map((e) => (
-        <div key={e.id}>
-          <h1>
-            {e.id}) {e.quote} - {e.author}
-          </h1>
-        </div>
-      ))}
-
-      
-    </>
-  );
-};
-
+    useEffect (()=>{
+        fetch('https://dummyjson.com/quotes')
+        .then(response => response.json())
+        .then(data => setInfo(data.quotes))
+        .catch(error => console.error(error))
+    },[])
+    
+    return (
+        <>
+        <img src={img} alt="Tickets" style={{ width: 200, height: 150, marginTop: 20 }} />
+        <h2>
+            {info.map((e,i)=>(
+                <p key={i}>
+                    {e.quote} <br />
+                    {e.author}
+                </p>
+            ))}
+        </h2>
+        </>
+    )
+}
 export default Quote;
